7922da32ec5ebef632c287043ca91f5eb4af0e61
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-03-05 18:07:39 +0000

    jsn.br

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,23 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
+	const [todoList, setTodoList] = useState([]);
+	const getTodos = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/JairoAriza")
+			.then(response => response.json())
+			.then(res => setTodoList(res));
+	};
+	useEffect(() => {
+		getTodos();
+	}, []);
+	console.log(todoList);
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card bg-danger text-center mt-5">
+			{todoList.map((item, index) => (
+				<p key={index}>{item.label}</p>
+			))}
 		</div>
 	);
 }
